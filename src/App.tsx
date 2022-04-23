708c6237a102a15238f17c7ae60d71213cc8e3a4
gabrielcode7 <gabrielbelon.junior@hotmail.com>
2022-04-23 18:27:54 -0300

    Entendendo Jsx e sem Jsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,45 +1,12 @@
-import { useState } from 'react'
-import logo from './logo.svg'
-import './App.css'
+import React from 'react';
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () =>{
+ // return React.createElement('div', null, 'Meu nome é Gabriel');
 
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 2)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.tsx</code> and save to test HMR updates | Gabriel Belon.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
-    </div>
-  )
+ //jsx
+  //return (<div>Meu nome é gabriel 2.0</div>);
+  //return (<a href="https://google.com">Clique aqui</a>)
 }
 
-export default App
+
+export default App;
